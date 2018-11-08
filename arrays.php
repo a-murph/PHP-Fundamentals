c052632423ff6cffa2690b9db3d2a3877d35f853
Andrew Murphy <ajmurph544@gmail.com>
2018-11-08 15:48:05 -0500

    first few PHP lessons

---
+++ arrays.php
@@ -0,0 +1,14 @@
+<?php
+
+$guitars = ['Vela', 'Explorer', 'Strat'];
+
+print_r($guitars);
+
+$guitars2 = [
+	'prs' => 'Vela',
+	'gibson' => 'Explorer',
+	'fender' => 'Strat'
+];
+
+print_r($guitars2);
+print_r($guitars2['prs']);
---
+++ conditions.php
@@ -0,0 +1,11 @@
+<?php
+
+$result = 1 < 3;
+
+echo $result;
+
+if ($result == true) {
+	echo "The statement is true";
+} else {
+	echo "The statement is true";
+}
